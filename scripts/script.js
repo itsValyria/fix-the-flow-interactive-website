f4dea19a8c8c43a5c44ac7e1bda67e535923c1b3
Valyria <76444716+itsValyria@users.noreply.github.com>
2023-12-08 09:19:07 +0100

    updated javascript

--- scripts/script.js
+++ scripts/script.js
@@ -8,4 +8,44 @@ window.addEventListener('load', () => {
   });
 });
 
-// Contact form
+// Contact form | No input
+let vnaam = document.getElementById('vnaam');
+vnaam.addEventListener('blur', () => {
+  checkIfEmpty(vnaam);
+});
+
+let anaam = document.getElementById('anaam');
+anaam.addEventListener('blur', () => {
+  checkIfEmpty(anaam);
+});
+
+let email = document.getElementById('email');
+email.addEventListener('blur', () => {
+  checkIfEmpty(email);
+});
+
+let type = document.getElementById('type');
+type.addEventListener('blur', () => {
+  if(type.value == 'choosehere') {
+    type.classList.add('form__invalid--visible')
+  } else {
+    type.classList.remove('form__invalid--visible')
+  }
+});
+
+let subject = document.getElementById('subject');
+subject.addEventListener('blur', () => {
+  checkIfEmpty(subject);
+});
+
+function checkIfEmpty(element) {
+  if(element.value == null || element.value == '') {
+    element.classList.add('form__invalid--visible')
+    document.getElementById('invalidFieldMessage').classList.add('form__invalid--message');
+  } else {
+    element.classList.remove('form__invalid--visible')
+    document.getElementById('invalidFieldMessage').classList.remove('form__invalid--message');
+  }
+}
+ 
+// Contact form | E-mail invalid
